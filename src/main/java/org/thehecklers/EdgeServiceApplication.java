67808295de39ab018a7bc43a14d4981042e2b0d8
Mark Heckler <mark.heckler@gmail.com>
2016-02-15 19:55:33 -0600

    Initial commit.

---
+++ src/main/java/org/thehecklers/EdgeServiceApplication.java
@@ -0,0 +1,16 @@
+package org.thehecklers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EdgeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeServiceApplication.class, args);
+	}
+}
